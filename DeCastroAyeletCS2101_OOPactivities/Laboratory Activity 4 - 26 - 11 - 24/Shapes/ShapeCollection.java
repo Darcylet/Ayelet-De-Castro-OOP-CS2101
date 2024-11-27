98d8e5dbb4550eb12de332436448cb3bb38a20a2
Ayelet D'arcy De Castro <165733384+Darcylet@users.noreply.github.com>
2024-11-27 13:08:41 +0800

    Add files via upload

---
+++ DeCastroAyeletCS2101_OOPactivities/Laboratory Activity 4 - 26 - 11 - 24/Shapes/ShapeCollection.java
@@ -0,0 +1,16 @@
+class ShapeCollection {
+    private final Shape[] shapes;
+
+    public ShapeCollection(Shape[] shapes) {
+        this.shapes = shapes;
+    }
+
+    public void getPropertyValues() {
+        for (Shape shape : shapes) {
+            System.out.println(shape.printShapeType());
+            System.out.printf("Area: %.2f\n", shape.getArea());
+            System.out.printf("Perimeter: %.2f\n", shape.getPerimeter());
+            System.out.println();
+        }
+    }
+}
---
+++ DeCastroAyeletCS2101_OOPactivities/Laboratory Activity 4 - 26 - 11 - 24/Shapes/Square.java
@@ -0,0 +1,19 @@
+class Square implements Shape{
+    private final double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+
+    @Override
+    public String printShapeType() {return "Square";}
+}
